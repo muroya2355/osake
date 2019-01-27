f9fbd1c358b947b8ae90907a36a22370528f6562
muroya2355 <ishimuroya.masato@gmail.com>
2019-01-27 11:24:32 +0900

    validation 追加

---
+++ go/utils/parametervalidator.go
@@ -0,0 +1,56 @@
+package utils
+
+import "regexp"
+import "strconv"
+
+// IsEmpty : 引数が空文字のとき、true を返す
+func IsEmpty(parameter string) bool {
+	return parameter == ""
+}
+
+// IsStringLengthWithin : 引数の文字列長がmin~maxである場合、true を返す
+func IsStringLengthWithin(parameter string, min int, max int) bool {
+	if parameter == "" {
+		return true
+	}
+	if max < min {
+		return false
+	}
+	return min <= len(parameter) && len(parameter) <= max
+}
+
+// IsHankakuAlphaNum : 引数の文字列が半角英数字のみの場合、trueを返す
+func IsHankakuAlphaNum(parameter string) bool {
+	if parameter == "" {
+		return true
+	}
+	flag := true
+	for _, char := range parameter {
+		f, _ := regexp.MatchString("[A-Za-z0-9]", string(char))
+		flag = flag && f
+	}
+	return flag
+}
+
+// エラーメッセージ一覧
+func Error_login() string {
+	return "ログインに失敗しました。ユーザID、パスワードを確認してください。"
+}
+func Error_required1(a string) string {
+	return a + "を入力してください。"
+}
+func Error_required2(a, b string) string {
+	return a + "には" + b + "を入力してください。"
+}
+func Error_length(a string, from, to int) string {
+	return a + "には " + strconv.Itoa(from) + " 文字以上、" + strconv.Itoa(to) + " 文字以下で入力してください。"
+}
+func Error_select(a string) string {
+	return a + "を選択してください。"
+}
+func Error_lock() string {
+	return "該当の商品は他のユーザに更新されました。再度、商品詳細画面を表示してから、更新業務を行ってください。"
+}
+func Error_system() string {
+	return "システムエラーが発生しました。"
+}
